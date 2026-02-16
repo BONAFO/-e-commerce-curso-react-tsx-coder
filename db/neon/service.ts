eee118e52ffcf0fcf7b8e0e1bee9f6f6344c3063
BONAFO <bgbcnnvb@gmail.com>
2026-02-16 01:08:51 -0300

    Adding Neon

--- db/neon/service.ts
+++ db/neon/service.ts
@@ -1,6 +1,8 @@
 import { neon } from "@neondatabase/serverless";
 
 export async function testConnection() {
+  console.log("sda", process.env.ENV_TEST!);
+
   const sql = neon(process.env.DATABASE_URL!);
   const result = await sql`SELECT NOW();`;
   console.log("DB responde:", result);
