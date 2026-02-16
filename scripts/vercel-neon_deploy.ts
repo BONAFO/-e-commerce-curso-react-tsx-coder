6dd2ac1cc0a7decc0e6dadd66ad9853449c2256b
BONAFO <bgbcnnvb@gmail.com>
2026-02-16 03:21:59 -0300

    Adding Neon Poblando Products

--- scripts/vercel-neon_deploy.ts
+++ scripts/vercel-neon_deploy.ts
@@ -1,7 +1,29 @@
 import "dotenv/config";
 import { neon } from "@neondatabase/serverless";
+import products from "./products.json"
+
 
 const sql = neon(process.env.DATABASE_URL!);
+
+
+
+
+async function insertProducts() {
+  try {
+    for (const p of products) {
+      await sql`
+        INSERT INTO Games (name, img, price, description, stock, category)
+        VALUES (${p.name}, ${p.img}, ${p.price}, ${p.description}, ${p.stock}, ${p.category})
+        ON CONFLICT DO NOTHING;
+      `;
+      console.log(`✅ Insertado: ${p.name}`);
+    }
+  } catch (err) {
+    console.error("❌ Error insertando productos:", err);
+  }
+}
+
+
 
 async function deploy() {
   try {
@@ -45,6 +67,8 @@ async function deploy() {
       products INTEGER[][] NOT NULL
     );`;
     console.log("Orders -> Table",resp);
+
+    await insertProducts();
 
   } catch (err) {
     console.error("❌ Error creando tablas:", err);
