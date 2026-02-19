81e22439d993848c986bfefb10180cff869f672e
BONAFO <bgbcnnvb@gmail.com>
2026-02-19 19:23:36 -0300

    Added GET Products Route id y name middlewares

--- middlewares/products.ts
+++ middlewares/products.ts
@@ -2,27 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 import sql from "@/db/neon/db";
 import GameType from "@/types/games";
 
-
-export async function getProductByID(
-  id: string,
-): Promise<{ status: number; data: GameType[] }> {
+export async function getProductByID(id: string) {
   try {
     const rows = await sql`SELECT * FROM Games WHERE id = ${id};`;
-    return { status: 200, data: rows as GameType[] };
+    return NextResponse.json({ status: 200, data: rows as GameType[] });
   } catch (err) {
     console.error("❌ Error en getProductByID:", err);
     return { status: 500, data: [] };
   }
 }
 
-export async function getProductsByName(
-  name: string,
-): Promise<{ status: number; data: GameType[] }> {
+export async function getProductsByName(name: string) {
   try {
     const rows = await sql`
       SELECT * FROM Games WHERE LOWER(name) LIKE ${"%" + name.toLowerCase() + "%"};
     `;
-    return { status: 200, data: rows as GameType[] };
+    return NextResponse.json({ status: 200, data: rows as GameType[] });
   } catch (err) {
     console.error("❌ Error en getProductsByName:", err);
     return { status: 500, data: [] };
