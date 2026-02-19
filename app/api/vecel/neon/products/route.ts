ffb0b59afbda731e07ced453db5db3c10a03d376
BONAFO <bgbcnnvb@gmail.com>
2026-02-19 19:29:41 -0300

    Added GET Products Route id y name middlewares

--- app/api/vecel/neon/products/route.ts
+++ app/api/vecel/neon/products/route.ts
@@ -9,7 +9,6 @@ import {
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  try {
     const { searchParams } = new URL(req.url);
 
     if (searchParams.get("id")) {
@@ -19,8 +18,4 @@ export async function GET(req: NextRequest) {
     } else {
       return await getProducts();
     }
-  } catch (err) {
-    console.error("❌ Error en GET /products:", err);
-    return NextResponse.json({ status: 500, data: [] });
-  }
 }
