9991b43e8d12d800b64432976dcea113c16d5b65
BONAFO <bgbcnnvb@gmail.com>
2026-02-19 19:17:14 -0300

    Added GET Products Route id y name middlewares

--- app/api/vecel/neon/products/route.ts
+++ app/api/vecel/neon/products/route.ts
@@ -1,15 +1,21 @@
+import {
+  getProductByID,
+  getProducts,
+  getProductsByName,
+} from "@/middlewares/products";
 import { NextRequest, NextResponse } from "next/server";
-import sql from "@/db/neon/db";
-import GameType from "@/types/games";
-import { NextApiRequest } from "next";
 
-//mofificar para poder obtener el query
-export async function GET(req: NextApiRequest) {
-  
-  
+export async function GET(req: NextRequest) {
   try {
-    const rows = await sql`SELECT * FROM games;`;
-    return NextResponse.json({ status: 200, data: rows as GameType[] , req: [req.query]});
+    const { searchParams } = new URL(req.url);
+
+    if (searchParams.get("id")) {
+      return await getProductByID(searchParams.get("id") as string);
+    } else if (searchParams.get("name")) {
+      return await getProductsByName(searchParams.get("name") as string);
+    } else {
+      return await getProducts();
+    }
   } catch (err) {
     console.error("❌ Error en GET /products:", err);
     return NextResponse.json({ status: 500, data: [] });
