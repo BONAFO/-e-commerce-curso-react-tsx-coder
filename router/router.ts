a8fd17c6ca00fc69179e24ca0e1f8e00d14a4433
BONAFO <bgbcnnvb@gmail.com>
2026-02-19 04:06:41 -0300

    Added GET Products Route id y name

--- router/router.ts
+++ router/router.ts
@@ -1,23 +1,12 @@
-import { MODE } from "@/db/service";
-
-export const useQuery = ["neon"];
-
-// const isQueryMode = useQuery.includes(MODE);
-const isQueryMode = useQuery.includes("neon");
-
 export const routes = {
   mainPage: "/",
   mainPageCategories: "/categorias",
-  mainPageCategorie: (id: string | number) =>
-    `/categorias/${isQueryMode ? "?id=" : ""}${id}`,
-  productDetail: (productID: string | number) =>
-    `/game/${isQueryMode ? "?productID=" : ""}${productID}`,
+  mainPageCategorie: (id: string | number) => `/categorias/${id}`,
+  productDetail: (productID: string | number) => `/game/${productID}`,
   productBill: "/cart/bill",
   productPay: "/cart/pay",
   contact: "/contact",
-  newOrder: (orderID: string | number) =>
-    `/order/new/${isQueryMode ? "?orderID=" : ""}${orderID}`,
-  showOrder: (orderID: string | number) =>
-    `/order/${isQueryMode ? "?orderID=" : ""}${orderID}`,
+  newOrder: (orderID: string | number) => `/order/new/${orderID}`,
+  showOrder: (orderID: string | number) => `/order/${orderID}`,
   searchOrder: "/order",
 };
