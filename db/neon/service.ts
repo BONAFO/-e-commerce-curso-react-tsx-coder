0f4a92be4e28467cea0323f93d96faf555c6f87e
BONAFO <bgbcnnvb@gmail.com>
2026-02-19 18:49:08 -0300

    Added GET Products Route id y name

--- db/neon/service.ts
+++ db/neon/service.ts
@@ -13,44 +13,44 @@ const getProducts = async () => {
   }
 };
 
-// Obtener producto por ID
-const getProductsByID = async (
-  id: number | string,
-): Promise<{ status: number; data: GameType[] }> => {
-  try {
-    const resp = await axios.get(`${vecel_neon_route}products/${id}`);
-    return resp.data;
-  } catch (err) {
-    console.error("❌ Error getProductByID:", err);
-    return { status: 500, data: [] };
-  }
-};
+// // Obtener producto por ID
+// const getProductsByID = async (
+//   id: number | string,
+// ): Promise<{ status: number; data: GameType[] }> => {
+//   try {
+//     const resp = await axios.get(`${vecel_neon_route}products/${id}`);
+//     return resp.data;
+//   } catch (err) {
+//     console.error("❌ Error getProductByID:", err);
+//     return { status: 500, data: [] };
+//   }
+// };
 
-export const cosa =async ()=>{
-  const resp = await axios.get(`${vecel_neon_route}products?id=1`);
-  console.log("cpsa",resp.data);
+// export const cosa =async ()=>{
+//   const resp = await axios.get(`${vecel_neon_route}products?id=1`);
+//   console.log("cpsa",resp.data);
   
-}
+// }
 
-cosa()
+// cosa()
 
-// Buscar productos por nombre
-const getProductsByName = async (
-  name: string,
-): Promise<{ status: number; data: GameType[] }> => {
-  try {
-    const resp = await axios.get(`${vecel_neon_route}products/${name}`);
-    return resp.data;
-  } catch (err) {
-    console.error("❌ Error getProductsByName:", err);
-    return { status: 500, data: [] };
-  }
-};
+// // Buscar productos por nombre
+// const getProductsByName = async (
+//   name: string,
+// ): Promise<{ status: number; data: GameType[] }> => {
+//   try {
+//     const resp = await axios.get(`${vecel_neon_route}products/${name}`);
+//     return resp.data;
+//   } catch (err) {
+//     console.error("❌ Error getProductsByName:", err);
+//     return { status: 500, data: [] };
+//   }
+// };
 
 export default {
   getProducts,
-  getProductsByName,
-  getProductsByID,
+  // getProductsByName,
+  // getProductsByID,
   //   getProductsByCatID,
   //   getProductsByCatName,
   //   getCategories,
