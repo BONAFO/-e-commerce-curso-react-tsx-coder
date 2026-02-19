db19395ea830fb72cfcd2e66b3a8ed7bc22a45d1
BONAFO <bgbcnnvb@gmail.com>
2026-02-19 03:07:08 -0300

    Added GET Products Route id y name

--- app/api/vecel/neon/products/[param]/route.ts
+++ app/api/vecel/neon/products/[param]/route.ts
@@ -1,45 +1,14 @@
-import sql from "@/db/neon/db";
-import GameType from "@/types/games";
-import { NextResponse, NextRequest } from "next/server";
-
-async function getProductByID(
-  id: string,
-): Promise<{ status: number; data: GameType[] }> {
-  try {
-    const rows = await sql`SELECT * FROM Games WHERE id = ${id};`;
-    return { status: 200, data: rows as GameType[] };
-  } catch (err) {
-    console.error("❌ Error en getProductByID:", err);
-    return { status: 500, data: [] };
-  }
-}
-
-async function getProductsByName(
-  name: string,
-): Promise<{ status: number; data: GameType[] }> {
-  try {
-    const rows = await sql`
-      SELECT * FROM Games WHERE LOWER(name) LIKE ${"%" + name.toLowerCase() + "%"};
-    `;
-    return { status: 200, data: rows as GameType[] };
-  } catch (err) {
-    console.error("❌ Error en getProductsByName:", err);
-    return { status: 500, data: [] };
-  }
-}
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  req: Request,
-  //@ts-ignore
-  { params }: { params: { param: string } },
+  request: NextRequest,
+  { params }: { params: { param: string } }
 ) {
-  const { param } = params;
 
-  if (/^\d+$/.test(param)) {
-    const resp = await getProductByID(param);
-    return NextResponse.json(resp);
-  }
-
-  const resp = await getProductsByName(param);
-  return NextResponse.json(resp);
+  console.log(params);
+  
+  return NextResponse.json({
+    status: 200,
+    param: params.param,
+  });
 }
