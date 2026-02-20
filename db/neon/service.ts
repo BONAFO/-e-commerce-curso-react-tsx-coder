53ba72886aabc498686464b1a907c05bb4e26c1d
BONAFO <bgbcnnvb@gmail.com>
2026-02-20 03:23:58 -0300

    Added GET Order Data WIP

--- db/neon/service.ts
+++ db/neon/service.ts
@@ -93,6 +93,6 @@ export default {
   getProductsByCatID,
   getProductsByCatName,
   getCategories,
-  //   saveSell,
+    saveSell,
   //   getOrder,
 };
--- db/service.ts
+++ db/service.ts
@@ -1,32 +1,13 @@
 import mock2 from "./mock/service";
-import neon from "./neon/service";          
-
-
+import neon from "./neon/service";
 
 export const MODE = "mock";
 
-const mock = mock2 
-//@ts-ignore
-mock.getProducts = neon.getProducts;
-//@ts-ignore
-mock.getProductsByID = neon.getProductsByID;
-//@ts-ignore
-mock.getProductsByName = neon.getProductsByName;
-// //@ts-ignore
-mock.getCategories = neon.getCategories;
-//@ts-ignore
-mock.getProductsByCatID = neon.getProductsByCatID;
-//@ts-ignore
-mock.getProductsByCatName = neon.getProductsByCatName;
-
-
-
-
-
-
-
-
-
+const mock = mock2;
+Object.keys(neon).map((k) => {
+  //@ts-ignore
+  mock[k] = neon[k];
+});
 
 const service = {
   mock,
